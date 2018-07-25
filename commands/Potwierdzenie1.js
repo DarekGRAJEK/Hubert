7fa23d4985d80ee6dda495b7786d8d0d008f28f5
Darek <wojak1011@gmail.com>
2018-07-25 20:14:37 +0200

    2

--- commands/Potwierdzenie1.js
+++ commands/Potwierdzenie1.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    await(rUser.addRole(471737400939380737));
+    await(rUser.addRole("Regulamin √"));
     message.delete().catch(O_o=>{});
 
 
