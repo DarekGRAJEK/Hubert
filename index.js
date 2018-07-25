7a79bc73208f1ad30b0e2cb1436a716d17d4cee4
Darek <wojak1011@gmail.com>
2018-07-25 19:26:08 +0200

    2

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const botconfig = require("./botconfig.json");
-const tokenfile = require("./token.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
+const token = process.env.token;
 bot.commands = new Discord.Collection();
+
 
 fs.readdir("./commands/", (err, files) => {
 
