af6b11e5f36792e9b60963b1863d3eeaf9e6444c
DarekGRAJEK <41697254+DarekGRAJEK@users.noreply.github.com>
2018-07-28 00:06:15 +0200

    ppp

--- commands/Potwierdzenie1.js
+++ commands/Potwierdzenie1.js
@@ -2,13 +2,11 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     message.delate();
-    let rUser = message.author.id;
-    if(!rUser) return message.replay("Error 1!")
-    let gRole = Discord.guild.roles.find(`name`, "Regulamin √");
+    gRole = Discord.guild.roles.get(`name`, "Regulamin √");
     if(!gRole) return message.replay("Error 2!")
 
-    if (rUser.role.has(gRole.id));
-    await(rUser.addRole(gRole.id));
+    if (message.author.id.role.has(gRole.id));
+    await(message.author.id.addRole(gRole.id));
 
 
 }
