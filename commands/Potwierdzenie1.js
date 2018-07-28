d971cdbc47f907958fe383ce6ad0b7a538b9cf2d
DarekGRAJEK <41697254+DarekGRAJEK@users.noreply.github.com>
2018-07-28 03:09:21 +0200

    34397

--- commands/Potwierdzenie1.js
+++ commands/Potwierdzenie1.js
@@ -2,10 +2,11 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     await message.delete();
+    let name = message.author();
     let gRole = Discord.guild.roles.get(`name`, "Regulamin √");
     if(!gRole) return message.replay("Error 2!")
 
-    await(message.author.addRole(gRole.id));
+    await(name.addRole(gRole.id));
 
 
 }
