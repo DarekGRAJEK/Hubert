543b8031f25684580fface000383c7d12a3fdbdf
DarekGRAJEK <41697254+DarekGRAJEK@users.noreply.github.com>
2018-07-28 03:51:51 +0200

    35295

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ fs.readdir("./commands/", (err, files) => {
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("tutorials on TSC", {type: "WATCHING"});
+  bot.user.setActivity("Looking At Discord", {type: "WATCHING"});
 
 });
 
