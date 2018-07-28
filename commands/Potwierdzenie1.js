cbf0776f707f7b3ac152ed2781582d51420c1b51
DarekGRAJEK <41697254+DarekGRAJEK@users.noreply.github.com>
2018-07-28 04:05:10 +0200

    316279

--- commands/Potwierdzenie1.js
+++ commands/Potwierdzenie1.js
@@ -2,11 +2,12 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     await message.delete();
-    let name = message.author.id;
+    let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+    if(rMember) return message.reply("Nie mogę znaleść twojego nicku.");
     let gRole = Discord.guild.roles.get(`name`, "Regulamin✅");
     if(!gRole) return message.replay("Error 2!")
 
-    await(name.addRole(gRole.id));
+    await(rMember.addRole(gRole.id));
 
 
 }
