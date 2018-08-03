57ee428cc4da025276e3dbea91b15433972c78f7
Darek <wojak1011@gmail.com>
2018-08-03 21:21:16 +0200

    3123

--- commands/clear.js
+++ commands/clear.js
@@ -3,6 +3,7 @@ const errors = require("../utility/error.js");
 
 module.exports.run = async (bot, message, args) => {
 
+  await message.delete();
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
   if(!args[0]) return message.channel.send("Noo...");
   message.channel.bulkDelete(args[0]).then(() => {
