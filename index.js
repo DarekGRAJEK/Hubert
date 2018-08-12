cbc43987a4a4c8a14f12f9a445a3ad289cbbb7d0
Darek <wojak1011@gmail.com>
2018-08-12 19:25:30 +0200

    2165

--- index.js
+++ index.js
@@ -41,14 +41,17 @@ bot.on("message", async message => {
 
 let coinAmt = Math.floor(Math.random() * 1000) + 1;
 let baseAmt = Math.floor(Math.random() * 1000) + 1;
+
 console.log(`${coinAmt} ; ${baseAmt}`);
 if(coinAmt === baseAmt){
 coins[message.author.id] = {
 coins: coins[message.author.id].coins + coinAmt
 };
+
 fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
 if (err) console.log(err)
 });
+
 let coinEmbed = new Discord.RichEmbed()
 .setAuthor(message.author.username)
 .setColor("#0000FF")
