6fe129c467db2b9f36f6751802334d49face24c7
DarekGRAJEK <41697254+DarekGRAJEK@users.noreply.github.com>
2018-08-14 19:28:26 +0200

    2313

--- commands/warns.js
+++ commands/warns.js
@@ -5,12 +5,14 @@ let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
 
+  await message.delete();
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't do that.");
   let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
   if(!wUser) return message.reply("Couldn't find them yo");
   let warnlevel = warns[wUser.id].warns;
-
-  message.reply(`<@${wUser.id}> has ${warnlevel} warnings.`);
+  if(!warnlevel) return message.reply("That player have any warns");
+  
+  await message.reply(`<@${wUser.id}> has ${warnlevel} warnings.`);
 
 }
 
